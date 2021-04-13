8960df33ae8aba69e6f34138e871014de05003c9
barkin <c.barkinozer@gmail.com>
2021-04-13 15:11:03 +0300

    Added and Applied Entity Framework

--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,18 @@ namespace EntityLayer.Concrete
 {
     public class Heading
     {
+        [Key]
         public int HeadingID { get; set; }
+        
+        [StringLength(50)]
         public string HeadingName { get; set; }
+        
+        //No constraint
         public DateTime HeadingDate { get; set; }
 
-        public int CategoryID { get; set; } //key
+        //Relation area
+
+        public int CategoryID { get; set; } //foreign key
         public virtual Category Category { get; set; }
 
         public int WriterID { get; set; }

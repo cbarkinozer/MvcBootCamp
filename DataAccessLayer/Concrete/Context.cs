8b09db79e68e437a59f734016bb33d638d824a94
barkin <c.barkinozer@gmail.com>
2021-04-17 18:58:38 +0300

    Add DataAccessLayer Concrete File Add DataAccessLayer Context in Concrete File

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        /*The name is plural because to avoid confusion.
+         * "About" is the class in the project, but "Abouts" is the table in the database*/
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}

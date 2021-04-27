9a8c7670c9a919d2a040f5f31926c86cbf07aea8
barkin <c.barkinozer@gmail.com>
2021-04-27 16:13:37 +0300

    Add Views\Category\GetCategoryList.cshtml Modify CategoryController GetCategoryList() added References added EntityFramework added to  MvcBootCamp

--- MvcBootCamp/Controllers/CategoryController.cs
+++ MvcBootCamp/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,19 @@ using System.Web.Mvc;
 
 namespace MvcBootCamp.Controllers
 {
+    
     public class CategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager();
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList() 
+        {
+            var categoryValues = categoryManager.GetAllBL();
+            return View(categoryValues);
         }
     }
 }

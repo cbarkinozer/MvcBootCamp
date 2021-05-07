0b891aad2c044a6e8af35cd614d3362e6eb0ff1d
barkin <c.barkinozer@gmail.com>
2021-05-07 18:08:53 +0300

    A AddCategory M  Index

--- MvcBootCamp/Controllers/AdminCategoryController.cs
+++ MvcBootCamp/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +14,34 @@ namespace MvcBootCamp.Controllers
     public class AdminCategoryController : Controller
     {
         // GET: AdminCategory
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            var categoryValues = categoryManager.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator= new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(category);
+            if (results.IsValid) 
+            {
+                categoryManager.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
